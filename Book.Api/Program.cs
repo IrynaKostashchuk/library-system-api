3a50f8079998e326d81f0faed2ad61840b129f66
IrynaKostashchuk <kostashchuk.iryna@chnu.edu.ua>
2023-11-14 04:47:23 +0200

    added

--- Book.Api/Program.cs
+++ Book.Api/Program.cs
@@ -1,6 +1,40 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+using WebApiEF.Data;
+using WebApiEF.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ILibraryService, LibraryService>();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Register database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
