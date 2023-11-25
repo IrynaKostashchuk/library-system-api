344a4ec8a6e2b58c402a22c73e13c952e295bbc4
IrynaKostashchuk <kostashchuk.iryna@chnu.edu.ua>
2023-11-25 17:13:09 +0200

    jwt

---
+++ JWT/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using JWT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT.Contexts;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
---
+++ JWT/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using JWT.Models;
+
+namespace JWT.Services
+{
+    public interface IUserService
+    {
+        Task<string> RegisterAsync(RegisterModel model);
+        Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+
+        Task<AuthenticationModel> RefreshTokenAsync(string jwtToken);
+
+        bool RevokeToken(string token);
+        ApplicationUser GetById(string id);
+    }
+}
