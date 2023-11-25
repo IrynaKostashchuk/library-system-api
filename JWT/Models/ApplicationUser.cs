81da4f4a57c587ee6b7dd28602ea232380bfd616
IrynaKostashchuk <kostashchuk.iryna@chnu.edu.ua>
2023-11-25 17:14:46 +0200

    jwt

---
+++ JWT/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using JWT.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace JWT.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public List<RefreshToken> RefreshTokens { get; set; }
+}
